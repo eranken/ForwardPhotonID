2e62a5c8876d4a5ac1ab9ac5be28209d54815e08
eranken <eranken@ur.rochester.edu>
2020-10-28 16:18:03 +0100

    had to edit locally because of internet problems

--- checker.py
+++ checker.py
@@ -1,26 +1,26 @@
 import ROOT as r
 from ROOT import TFile, TH1D, TH2D, TCanvas, TLegend, TTree, gPad, gStyle, gROOT
 
-numevents=10000000
-region = '2'
+numevents=1000000
+region = 2
 
-fi = TFile('TrainIn/CutTMVAregion'+region+'.root', 'READ')
+fi = TFile('TrainIn/CutTMVAregion'+str(region)+'.root', 'READ')
 
 t_S = fi.Get('t_S')
 t_B = fi.Get('t_B')
 
-histP_S=TH1D('IsoP_Shist','Iso P',100,0,6)
-histC_S=TH1D('IsoC_Shist','Iso C',100,0,6)
-histN_S=TH1D('IsoN_Shist','Iso N',100,0,6)
-histHOE_S=TH1D('HOE_Shist','H over E',100,0,0.1)
-histSieie_S=TH1D('Sieie_Shist','Sigma i#etai#eta',100,0,0.06)
+histP_S=TH1D('IsoP_Shist','Iso P region '+str(region),200,0,6)
+histC_S=TH1D('IsoC_Shist','Iso C region '+str(region),200,0,6)
+histN_S=TH1D('IsoN_Shist','Iso N region '+str(region),200,0,6)
+histHOE_S=TH1D('HOE_Shist','H over E region '+str(region),200,0,0.1)
+histSieie_S=TH1D('Sieie_Shist','Sigma i#etai#eta region '+str(region),200,0,0.06)
 
 
-histP_B=TH1D('IsoP_Bhist','IsoP_B',100,0,6)
-histC_B=TH1D('IsoC_Bhist','IsoC_B',100,0,6)
-histN_B=TH1D('IsoN_Bhist','IsoN_B',100,0,6)
-histHOE_B=TH1D('HOE_Bhist','HOE_B',100,0,0.1)
-histSieie_B=TH1D('Sieie_Bhist','Sieie_B',100,0,0.06)
+histP_B=TH1D('IsoP_Bhist','IsoP_B region '+str(region),200,0,6)
+histC_B=TH1D('IsoC_Bhist','IsoC_B region '+str(region),200,0,6)
+histN_B=TH1D('IsoN_Bhist','IsoN_B region '+str(region),200,0,6)
+histHOE_B=TH1D('HOE_Bhist','HOE_B region '+str(region),200,0,0.1)
+histSieie_B=TH1D('Sieie_Bhist','Sieie_B region '+str(region),200,0,0.06)
 
 # for entryNum in range(0,t_S.GetEntries()):
 for entryNum in range(0,numevents):
@@ -63,7 +63,7 @@ histC_S.SetFillColor(r.kRed)
 histC_B.Draw('hist same')
 histC_B.SetLineColor(r.kBlue)
 cC.SetLogy()
-cC.SaveAs('tests/isoC_lowEta'+region+'.pdf')
+cC.SaveAs('tests/isoCregion'+str(region)+'.pdf')
 
 cP = TCanvas()
 histP_S.Draw('hist')
@@ -72,7 +72,7 @@ histP_S.SetFillColor(r.kRed)
 cP.SetLogy()
 histP_B.Draw('hist same')
 histP_B.SetLineColor(r.kBlue)
-cP.SaveAs('tests/isoP_lowEta'+region+'.pdf')
+cP.SaveAs('tests/isoPregion'+str(region)+'.pdf')
 
 cN = TCanvas()
 histN_S.Draw('hist')
@@ -81,7 +81,7 @@ histN_S.SetFillColor(r.kRed)
 cN.SetLogy()
 histN_B.Draw('hist same')
 histN_B.SetLineColor(r.kBlue)
-cN.SaveAs('tests/isoN_lowEta'+region+'.pdf')
+cN.SaveAs('tests/isoNregion'+str(region)+'.pdf')
 
 cHOE = TCanvas()
 histHOE_S.Draw('hist')
@@ -90,7 +90,7 @@ histHOE_S.SetFillColor(r.kRed)
 cHOE.SetLogy()
 histHOE_B.Draw('hist same')
 histHOE_B.SetLineColor(r.kBlue)
-cHOE.SaveAs('tests/HoE_lowEta'+region+'.pdf')
+cHOE.SaveAs('tests/HoEregion'+str(region)+'.pdf')
 
 cSieie = TCanvas()
 histSieie_S.Draw('hist')
@@ -98,4 +98,21 @@ histSieie_S.SetLineColor(r.kRed)
 histSieie_S.SetFillColor(r.kRed)
 histSieie_B.Draw('hist same')
 histSieie_B.SetLineColor(r.kBlue)
-cSieie.SaveAs('tests/Sieie_lowEta'+region+'.pdf')
+cSieie.SaveAs('tests/Sieieregion'+str(region)+'.pdf')
+
+outfile = TFile('tests/TrainHistsregion'+str(region)+'.root','RECREATE')
+
+histC_S.Write()
+histN_S.Write()
+histP_S.Write()
+histSieie_S.Write()
+histHOE_S.Write()
+
+histC_B.Write()
+histN_B.Write()
+histP_B.Write()
+histSieie_B.Write()
+histHOE_B.Write()
+
+outfile.Write()
+outfile.Close()
